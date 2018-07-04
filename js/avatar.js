efdfe511edae025825ec376e769ca63e07188f16
Yulia Akutina <akutinayulia@gmail.com>
2018-07-05 02:11:07 +0300

    Реализовала перетаскивание, в хроме ошибки больше нет, но не работает в мозилле

--- js/avatar.js
+++ js/avatar.js
@@ -14,6 +14,8 @@
   var userPicElement = popupOpenElement.querySelector('img');
   var avatarChooserElement = userDialogElement.querySelector('#avatar');
   var avatarPreviewElement = userDialogElement.querySelector('.setup-user-pic');
+  var formElement = userDialogElement.querySelector('.setup-wizard-form');
+  var uploadElement = formElement.querySelector('.upload');
 
   // Проверка расширения файла
   var checkIfFileTypeIsCorrect = function (file) {
@@ -25,30 +27,71 @@
   };
 
   // Загрузка и отображение файла
-  var renderPreview = function (image, imageCopy, file) {
-    var reader = new FileReader();
+  var renderPreview = function (image, imageCopy, files) {
+    Array.from(files).forEach(function (file) {
+      var matches = checkIfFileTypeIsCorrect(file);
 
-    reader.addEventListener('load', function () {
-      image.src = reader.result;
-      imageCopy.src = reader.result;
+      if (matches) {
+        var reader = new FileReader();
+
+        reader.addEventListener('load', function () {
+          image.src = reader.result;
+          imageCopy.src = reader.result;
+        });
+
+        reader.readAsDataURL(file);
+      }
     });
-
-    reader.readAsDataURL(file);
-  };
-
-  // Отображение аватара
-  var renderAvatar = function (avatar) {
-    var matches = checkIfFileTypeIsCorrect(avatar);
-
-    if (matches) {
-      renderPreview(avatarPreviewElement, userPicElement, avatar);
-    }
   };
 
   var avatarChooserElementChangeHandler = function () {
-    var selectedAvatar = avatarChooserElement.files[0];
-    renderAvatar(selectedAvatar);
+    var selectedAvatar = avatarChooserElement.files;
+    renderPreview(avatarPreviewElement, userPicElement, selectedAvatar);
   };
+
+  // Добравление подсветки области перетаскивания
+  var addHighlight = function () {
+    avatarPreviewElement.classList.add('highlight');
+  };
+
+  // Удаление подсветки области перетаскивания
+  var removeHighlight = function () {
+    avatarPreviewElement.classList.remove('highlight');
+  };
+
+  var uploadElementDragenterHandler = function (evt) {
+    evt.preventDefault();
+    evt.stopPropagation();
+    addHighlight();
+  };
+
+  var uploadElementDragleaveHandler = function (evt) {
+    evt.preventDefault();
+    evt.stopPropagation();
+    removeHighlight();
+  };
+
+  var uploadElementDragoverHandler = function (evt) {
+    evt.preventDefault();
+    evt.stopPropagation();
+    addHighlight();
+  };
+
+  var uploadElementDropHandler = function (evt) {
+    evt.preventDefault();
+    evt.stopPropagation();
+    removeHighlight();
+
+    var data = evt.dataTransfer;
+    var draggedAvatar = data.files;
+
+    renderPreview(avatarPreviewElement, userPicElement, draggedAvatar);
+  };
+
+  uploadElement.addEventListener('dragenter', uploadElementDragenterHandler, false);
+  uploadElement.addEventListener('dragleave', uploadElementDragleaveHandler, false);
+  uploadElement.addEventListener('dragover', uploadElementDragoverHandler, false);
+  uploadElement.addEventListener('drop', uploadElementDropHandler, false);
 
   avatarChooserElement.addEventListener('change', avatarChooserElementChangeHandler);
 })();
